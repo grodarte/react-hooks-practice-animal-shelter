a7e915eceffc27b85bbeb5ec674c09c9ac4c2a9a
Gabrielle <ms.grodarte@gmail.com>
2024-04-26 14:38:32 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,31 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleChangeType(filterSelection){
+    setFilters({
+      ...filters,
+      type: filterSelection
+    })
+  }
+
+  function handleFindPetsClick(){
+    const filterBy = filters.type === "all" ? "" : `/?type=${filters.type}`
+    fetch(`http://localhost:3001/pets${filterBy}`)
+    .then(r=>r.json())
+    .then(animalData=>setPets(animalData))
+  }
+
+  function handleAdoptPet(petID){
+    const updatedPets = pets.map(pet=>{
+      if(pet.id === petID){
+        return {...pet, isAdopted: true}
+      } else {
+        return pet
+      }
+    })
+    setPets(updatedPets)
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +40,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleChangeType} onFindPetsClick={handleFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
