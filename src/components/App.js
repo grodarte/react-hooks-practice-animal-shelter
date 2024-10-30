7afd26f86113028d6ab377d77dfea6e42f441f10
Gabrielle <ms.grodarte@gmail.com>
2024-10-30 10:40:47 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,43 @@ import React, { useState } from "react";
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
+const API = `http://localhost:3001/pets`
+
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  function handleChangeType(e){
+    setFilters({
+      type: e.target.value
+    })
+    // change filters type state
+  }
+
+  function handleFindPetsClick(){
+    // fetches a list of pets using fetch based on filters state
+    // optional query parameter: i.e. /pets?type=cat
+    //set pet state here
+    const fetchUrl = filters["type"] === "all" ? API : `http://localhost:3001/pets?type=${filters.type}`
+
+    fetch(fetchUrl)
+    .then(r=>r.json())
+    .then(petData=>setPets(petData))
+  }
+
+  function handleAdoptPet(id){
+    // find matching id in pet array and set isAdopted to True
+    const updatedPets = pets.map(pet=>{
+      if(pet.id === id){
+        return {
+          ...pet,
+          isAdopted: true
+        }
+      } return pet
+    })
+
+    setPets(updatedPets)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +49,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleChangeType} onFindPetsClick={handleFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
